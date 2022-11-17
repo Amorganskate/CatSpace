3aff114ad6868a5d4e0823d4809d2be262b0b4bd
daniel stewart <daniel.stewart@sidearmsports.com>
2022-11-17 11:24:53 -0500

    custom color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
         chubbo: ["Chubbo", "sans-serif"],
         supreme: ["Supreme", "sans-serif"],
       },
+      colors: {
+        yellow: "#FFC700",
+        "light-blue": "#C6F1FF",
+        purple: "#C7B9FF",
+      },
     },
   },
   plugins: [],
