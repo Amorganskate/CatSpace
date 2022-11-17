ae06c30617d91f5a8062f925ebf7ebba343024cf
daniel stewart <daniel.stewart@sidearmsports.com>
2022-11-17 10:35:19 -0500

    borders/shadows

--- src/components/CatPost.vue
+++ src/components/CatPost.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div
+    class="w-[375px] min-h-[300px] border-4 border-black rounded-2xl shadow-[0.5rem_0.5rem_red] shadow-black"
+  >
+    cat post
+  </div>
 </template>
 
 <script>
