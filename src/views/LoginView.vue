0261368a0095a595cd65a95fa43cb9cc11735f7c
Alfred Morgan <AlfredEMorgan@gmail.com>
2022-11-17 14:50:51 -0500

    Added netlify widget

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -19,11 +19,52 @@
           placeholder="password"
         />
       </div>
-      <button class="login__button">log in</button>
+      <button
+        @click="triggerNetlifyIdentityAction('login')"
+        class="login__button"
+      >
+        log in
+      </button>
       <a class="login__forgot" href="#"> forgot password? </a>
     </div>
   </div>
 </template>
+
+<script setup>
+import netlifyIdentity from "netlify-identity-widget";
+
+netlifyIdentity.init({
+  apiUrl: "https://netlify-identity-demo-vue.netlify.com/.netlify/identity",
+  logo: true,
+});
+
+function triggerNetlifyIdentityAction(action) {
+  if (action == "login" || action == "signup") {
+    netlifyIdentity.open(action);
+    netlifyIdentity.on(action, (user) => {
+      this.currentUser = {
+        username: user.user_metadata.full_name,
+        email: user.email,
+        access_token: user.token.access_token,
+        expires_at: user.token.expires_at,
+        refresh_token: user.token.refresh_token,
+        token_type: user.token.token_type,
+      };
+      this.updateUser({
+        currentUser: this.currentUser,
+      });
+      netlifyIdentity.close();
+    });
+  } else if (action == "logout") {
+    this.currentUser = null;
+    this.updateUser({
+      currentUser: this.currentUser,
+    });
+    netlifyIdentity.logout();
+    this.$router.push({ name: "Home" });
+  }
+}
+</script>
 <style scoped>
 .login {
   width: 100%;
