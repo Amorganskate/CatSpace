7f708a236896940c846168cb1ed008f8eea2d929
daniel stewart <daniel.stewart@sidearmsports.com>
2022-11-17 10:50:04 -0500

    current

--- src/components/CatPost.vue
+++ src/components/CatPost.vue
@@ -2,7 +2,14 @@
   <div
     class="w-[375px] min-h-[300px] border-4 border-black rounded-2xl shadow-[0.5rem_0.5rem_red] shadow-black"
   >
-    cat post
+    <!-- Content container -->
+    <div class="p-6">
+      <!-- Avatar / Username row -->
+      <div class="flex">
+        <div class="bg-black w-9 h-9 rounded-full"></div>
+        <span></span>
+      </div>
+    </div>
   </div>
 </template>
 
