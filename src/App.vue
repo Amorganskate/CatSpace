3167f8565d3fe4c9336751569a641d964cc84539
Alfred Morgan <AlfredEMorgan@gmail.com>
2022-11-17 10:21:43 -0500

    Adding Forms

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    
-  </header>
-  <main>
-    <RouterView />
-  </main>
-  
+  <div class="background_color">
+    <header></header>
+    <main class="flex justify-center">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.background_color {
+  background-color: #c6f1ff;
+}
+</style>
