8142542e78b0c0342d7bf76126dad31699f7f3d6
Alfred Morgan <AlfredEMorgan@gmail.com>
2022-11-17 11:08:06 -0500

    Updated Cat feed to just an array, updated form view

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-full max-w-xs">
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h1 class="flex justify-center font-bold mb-4">Create A Post Feed</h1>
+
       <div class="mb-4">
         <label
           class="block text-gray-700 text-sm font-bold mb-2"
@@ -13,6 +15,7 @@
           id="username"
           type="text"
           placeholder="Username"
+          v-model="username"
         />
       </div>
       <div class="mb-6">
@@ -27,7 +30,8 @@
           id="imagePath"
           type="text"
           placeholder="Image Path"
-          />
+          v-model="imagePath"
+        />
       </div>
       <div class="mb-6">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="Caption">
@@ -38,13 +42,16 @@
           id="caption"
           type="text"
           placeholder="Caption"
+          v-model="caption"
         />
       </div>
       <div class="flex justify-center">
         <button
+          type="button"
           style="background-color: #ffc700"
-          class="text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        
+          class="text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          v-on:click="AddToJsonFile()"
+        >
           Add To Cat Feed
         </button>
       </div>
@@ -53,7 +60,17 @@
 </template>
 
 <script setup>
+import {ref} from 'vue'
+import catFeed from "../dummy_data/cat_feed.json"
+const username = ref('');
+const imagePath = ref('');
+const caption = ref('');
 
+function AddToJsonFile() {
+    var json = catFeed;
+
+    console.log(json)
+}
 </script>
 
 <style>
